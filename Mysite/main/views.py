b4f81496c8820104163a7a2d2cf2868b89a41d90
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-02-15 17:15:22 -0300

    Forms - BD (Django)

---
+++ Mysite/main/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from .models import ToDoList, Item
+from .forms import CreateNewList
+
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/base.html", {"ls":ls})
+
+
+def home(response):
+    return render(response, "main/home.html", {})
+
+
+def create(response):
+    if response.method == 'POST':
+        form = CreateNewList()
+        
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = ToDoList(name=n)
+            t.save()
+        
+        return HttpResponseRedirect("/%i", "%t.id")
+        
+    else:
+        form = CreateNewList()
+    return render(response, "main/create.html", {'form':form})
